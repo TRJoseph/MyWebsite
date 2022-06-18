8c0bc4ed9c7e6ee9cd7c08cafccb14db65a65d60
Thomas Joseph <trjosep@clemson.edu>
2022-06-18 14:22:38 -0400

    variable additions

--- index.js
+++ index.js
@@ -1,27 +1,43 @@
-document.getElementById("home").onmouseover = function() {
+// JS controller for the index page of the website
+
+var homeElement = document.getElementById("home");
+var aboutMeElement = document.getElementById("about-me");
+var projectElement = document.getElementById("projects");
+var workElement = document.getElementById("work-experience");
+
+// TODO: maybe change these hoverover commands to be the icon instead of the div for more accurate displaying of the side bar nav
+homeElement.onmouseover = function() {
     document.getElementById("homeTab").style.display = "";
+    homeElement.style.transform = "scale(1.2)";
 }
-document.getElementById("home").onmouseleave = function() {
+homeElement.onmouseleave = function() {
     document.getElementById("homeTab").style.display = "none";
+    homeElement.style.transform = "scale(1)";
 }
 
-document.getElementById("about-me").onmouseover = function() {
+aboutMeElement.onmouseover = function() {
     document.getElementById("aboutTab").style.display = "";
+    aboutMeElement.style.transform = "scale(1.2)";
 }
-document.getElementById("about-me").onmouseleave = function() {
+aboutMeElement.onmouseleave = function() {
     document.getElementById("aboutTab").style.display = "none";
+    aboutMeElement.style.transform = "scale(1)";
 }
 
-document.getElementById("projects").onmouseover = function() {
+projectElement.onmouseover = function() {
     document.getElementById("projectTab").style.display = "";
+    projectElement.style.transform = "scale(1.2)";
 }
-document.getElementById("projects").onmouseleave = function() {
+projectElement.onmouseleave = function() {
     document.getElementById("projectTab").style.display = "none";
+    projectElement.style.transform = "scale(1)";
 }
 
-document.getElementById("work-experience").onmouseover = function() {
+workElement.onmouseover = function() {
     document.getElementById("workTab").style.display = "";
+    workElement.style.transform = "scale(1.2)";
 }
-document.getElementById("work-experience").onmouseleave = function() {
+workElement.onmouseleave = function() {
     document.getElementById("workTab").style.display = "none";
+    workElement.style.transform = "scale(1)";
 }
