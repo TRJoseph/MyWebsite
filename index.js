301df6a2b15dfde2c3fcb6bc624b655d71f72c3a
Thomas Joseph <trjosep@clemson.edu>
2022-06-18 14:06:51 -0400

    navigation menu updates

---
+++ index.js
@@ -0,0 +1,27 @@
+document.getElementById("home").onmouseover = function() {
+    document.getElementById("homeTab").style.display = "";
+}
+document.getElementById("home").onmouseleave = function() {
+    document.getElementById("homeTab").style.display = "none";
+}
+
+document.getElementById("about-me").onmouseover = function() {
+    document.getElementById("aboutTab").style.display = "";
+}
+document.getElementById("about-me").onmouseleave = function() {
+    document.getElementById("aboutTab").style.display = "none";
+}
+
+document.getElementById("projects").onmouseover = function() {
+    document.getElementById("projectTab").style.display = "";
+}
+document.getElementById("projects").onmouseleave = function() {
+    document.getElementById("projectTab").style.display = "none";
+}
+
+document.getElementById("work-experience").onmouseover = function() {
+    document.getElementById("workTab").style.display = "";
+}
+document.getElementById("work-experience").onmouseleave = function() {
+    document.getElementById("workTab").style.display = "none";
+}
