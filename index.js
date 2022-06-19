5eeab1c550a65a2fe9ee291b873c0f45850d9812
Thomas Joseph <trjosep@clemson.edu>
2022-06-19 19:29:46 -0400

    rework/fix for navigation on safari browsers

--- index.js
+++ index.js
@@ -7,37 +7,37 @@ var workElement = document.getElementById("work-experience");
 
 // TODO: maybe change these hoverover commands to be the icon instead of the div for more accurate displaying of the side bar nav
 homeElement.onmouseover = function() {
-    document.getElementById("homeTab").style.display = "";
+    document.getElementById("homeTab").style.opacity = "1";
     homeElement.style.transform = "scale(1.2)";
 }
 homeElement.onmouseleave = function() {
-    document.getElementById("homeTab").style.display = "none";
+    document.getElementById("homeTab").style.opacity = "0";
     homeElement.style.transform = "scale(1)";
 }
 
 aboutMeElement.onmouseover = function() {
-    document.getElementById("aboutTab").style.display = "";
+    document.getElementById("aboutTab").style.opacity = "1";
     aboutMeElement.style.transform = "scale(1.2)";
 }
 aboutMeElement.onmouseleave = function() {
-    document.getElementById("aboutTab").style.display = "none";
+    document.getElementById("aboutTab").style.opacity = "0";
     aboutMeElement.style.transform = "scale(1)";
 }
 
 projectElement.onmouseover = function() {
-    document.getElementById("projectTab").style.display = "";
+    document.getElementById("projectTab").style.opacity = "1";
     projectElement.style.transform = "scale(1.2)";
 }
 projectElement.onmouseleave = function() {
-    document.getElementById("projectTab").style.display = "none";
+    document.getElementById("projectTab").style.opacity = "0";
     projectElement.style.transform = "scale(1)";
 }
 
 workElement.onmouseover = function() {
-    document.getElementById("workTab").style.display = "";
+    document.getElementById("workTab").style.opacity = "1";
     workElement.style.transform = "scale(1.2)";
 }
 workElement.onmouseleave = function() {
-    document.getElementById("workTab").style.display = "none";
+    document.getElementById("workTab").style.opacity = "0";
     workElement.style.transform = "scale(1)";
 }
